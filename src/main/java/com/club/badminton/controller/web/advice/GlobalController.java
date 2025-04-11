d867c432069145393ca1ec5afee0be3900a800f3
John Kim <john.donggyu.kim@gmail.com>
2025-04-11 09:18:36 +0900

    ControllerAdvice 위치 변경, 주석 추가

--- src/main/java/com/club/badminton/controller/web/advice/GlobalController.java
+++ src/main/java/com/club/badminton/controller/web/advice/GlobalController.java
@@ -1,4 +1,4 @@
-package com.club.badminton.controller.web;
+package com.club.badminton.controller.web.advice;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalController {
 
+    /**
+     * 모든 controller에서 현재 url를 view쪽으로 넘겨주는 역할.
+     * bodyHeader에서 선택된 메뉴 하이라이트시 사용됨.
+     */
     @ModelAttribute("currentPath")
     public String currentPath(HttpServletRequest request) {
         return request.getRequestURI();
--- src/main/java/com/club/badminton/controller/web/advice/GlobalExceptionHandler.java
+++ src/main/java/com/club/badminton/controller/web/advice/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.club.badminton.controller.web;
+package com.club.badminton.controller.web.advice;
 
 import com.club.badminton.exception.attachment.FileTooBigException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * controller에 접근하기전 터지는 에러를 잡아줌
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * 제한된 용량보다 큰 파일이 들어올경우
+     */
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public String handleMaxUploadSizeExceeded(MaxUploadSizeExceededException ex,
                                               HttpServletRequest request,
